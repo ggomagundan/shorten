4e7233707fa80c820b0a0388fda43162c4e0861d
kai <kai@ParkByeongSangui-MacBook-Air.local>
2015-02-08 21:36:34 +0900

    modify README files

--- lib/shorten_url.rb
+++ lib/shorten_url.rb
@@ -1,16 +1,15 @@
 # Simple bijective function
 # Basically encodes any integer into a base(n) string,
 # where n is ALPHABET.length.
-# Based on pseudocode from http://stackoverflow.com/questions/742013/how-to-code-a-url-shortener/742047#742047
 
 module ShortenUrl
   ALPHABET =
-    "2bkczKrLH5sB0wAimQjDSNRg1uWaTF93MV6Y8tneGIEdvZXPUxholqypC4OJf7"
-  #"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split(//)
-    
+  # "2bkczKrLH5sB0wAimQjDSNRg1uWaTF93MV6Y8tneGIEdvZXPUxholqypC4OJf7"
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split(//)
+
   # make your own alphabet using:
   # (('a'..'z').to_a + ('A'..'Z').to_a + (0..9).to_a).shuffle.join
-   
+
   def encode_url(i)
     # from http://refactormycode.com/codes/125-base-62-encoding
     # with only minor modification
@@ -23,7 +22,7 @@ module ShortenUrl
     end
     s.reverse
   end
-   
+
   def decode_url(s)
     # based on base2dec() in Tcl translation
     # at http://rosettacode.org/wiki/Non-decimal_radices/Convert#Ruby
